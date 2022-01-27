6d7b96353ac64692ad4ebddbf6609b70b97fa334
nascript <nascodefy@gmail.com>
2022-01-27 16:02:21 +0700

    node js practice documentations

---
+++ 1-introduction/first-app.js
@@ -0,0 +1,5 @@
+console.log('hello from the vs code')
+
+const fs = require('fs');
+
+fs.writeFileSync('nama file.txt', 'isi file')
---
+++ 2-basic-javascript/a. core-syntax/play.js
@@ -0,0 +1,9 @@
+var yourName = 'nasution';
+var yourAge = 12;
+var hasHobbies = false;
+
+function summarizeUser(userName, userAge, userHobbies) {
+  return `User Name is ${userName} and User Age is ${userAge} and userHobbies is ${userHobbies}`;
+}
+
+console.log(summarizeUser(yourName, yourAge, hasHobbies));
