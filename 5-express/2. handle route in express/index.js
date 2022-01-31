e9fba328230f7fbde9b09517056fde415c0a6dee
nascript <nascodefy@gmail.com>
2022-01-31 21:41:32 +0700

    [PART 5] parser

--- 5-express/2. handle route in express/index.js
+++ 5-express/2. handle route in express/index.js
@@ -1,17 +1,17 @@
 // === create server with express
-const http = require('http');
-const express = require('express');
-const app = express();
+const http = require('http')
+const express = require('express')
+const app = express()
 
 app.use('/add', (req, res, next) => {
-  console.log('second middleware');
-  res.send('<h2>Hello this is second routes</h2>');
-});
+  console.log('second middleware')
+  res.send('<h2>Hello this is second routes</h2>')
+})
 
 app.use('/', (req, res, next) => {
-  console.log('first middleware');
-  res.send('<h1>Welcome to express JS</h1>');
-});
+  console.log('first middleware')
+  res.send('<h1>Welcome to express JS</h1>')
+})
 
-const server = http.createServer(app);
-server.listen(3000);
+const server = http.createServer(app)
+server.listen(3000)
--- 5-express/3. exercise/index.js
+++ 5-express/3. exercise/index.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const express = require('express');
-const app = express();
+const http = require('http')
+const express = require('express')
+const app = express()
 
 // first middleware
 // app.use((req, res, next) => {
@@ -11,21 +11,21 @@ const app = express();
 
 // middleware
 app.use((req, res, next) => {
-  console.log('this is second middleware');
-  next();
-});
+  console.log('this is second middleware')
+  next()
+})
 
 // handle request
 app.use('/user', (req, res, next) => {
   res.send(
     '<h3>Hi, this is endpoint "/user" </h3> list of user <ul><li>nas</li><li>nas</li></ul>'
-  );
-});
+  )
+})
 
 app.use('/', (req, res, next) => {
-  console.log('this is first middleware');
-  res.send('<p>Hi, Welcome to my code <b>this is endpoint "/"</b></p>');
-});
+  console.log('this is first middleware')
+  res.send('<p>Hi, Welcome to my code <b>this is endpoint "/"</b></p>')
+})
 
-const server = http.createServer(app);
-server.listen(3000);
+const server = http.createServer(app)
+server.listen(3000)
